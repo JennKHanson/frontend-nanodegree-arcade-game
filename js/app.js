5af8b6fe4fe984c85d2be9807680cbbd909e068c
Jennifer-Hanson <jennkhanson@gmail.com>
2018-07-26 23:25:53 -0400

    walk through

--- js/app.js
+++ js/app.js
@@ -1,3 +1,91 @@
+
+class Entity {
+    constructor() {
+      this.sprite = 'images/';
+      this.x = 2;
+      this.y = 5;
+    }
+
+    update(dt) {
+      this.isOutOfBoundsX = this.x > 5;
+      this.isOutOfBoundsY = this.y < 1;
+    }
+
+    render() {
+      ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
+    }
+
+    checkCollisions(playerOrEnemy) {
+      if (this.y === playerOrEnemy.y) {
+        if (this.x >= playerOrEnemy.x - 0.5 && this.x <= playerOrEnemy.x + 0.5) {
+        return true;
+      }
+      else {
+        return false;
+      }
+
+    }
+}
+/*
+function checkCollisions(){
+  allEnemies.forEach(enemy => {
+    if (enemy.checkCollisions(player) || player.checkCollisions(enemy)) {
+      player.y = 5;
+      player.x = 2;
+
+    }
+  });
+}*/
+
+class Player extends Entity {
+      constructor() {
+      super();
+      this.sprite += 'char-boy.png';
+    }
+}
+
+  handleInput(input){
+    switch(input){
+      case 'left':
+        this.x = this.x > 0 ? this.x - 1 : this.x;
+        break;
+      case 'up':
+        this.y = this.y > 0 ? this.y - 1 : this.y;
+        break;
+      case 'right':
+        this.x = this.x < 4 ? this.x + 1 : this.x;
+        break;
+      case 'down':
+        this.y = this.y < 5 ? this.y + 1 : this.y;
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+class Enemy extends Entity {
+  constructor(x, y) {
+    super();
+    this.sprite += 'enemy-bug.png'
+    this.x = x;
+    this.y = y;
+  }
+}
+
+
+  update(dt){
+    super.update();
+    if(this.isOutOfBoundsX){
+      this.x = -1;
+    }
+    else {
+      this.x += dt;
+    }
+  }
+
+}
+/*
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -21,19 +109,33 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 // Now write your own player class
+var player = function(x, y){
+this.sprite = 'images/char-boy.png';
+this.x = x;
+this.y = y;
+}
+player();
+
+
 // This class requires an update(), render() and
 // a handleInput() method.
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+// var allEnemies = []; // my code..
 // Place the player object in a variable called player
+*/
 
+
+const player = new Player();
+const allEnemies =[...Array(3)].map((_,i)=> new Enemy(0, i + 1));
 
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+//Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
