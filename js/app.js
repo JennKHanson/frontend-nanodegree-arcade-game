e04b6fc38f186a2816b94f39ae737fc3488a4b9e
Jennifer-Hanson <jennkhanson@gmail.com>
2018-07-30 22:11:46 -0400

    saving changes

--- js/app.js
+++ js/app.js
@@ -1,25 +1,38 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+class Enemy {
+  constructor(){
     this.sprite = 'images/enemy-bug.png';
-};
+    this.x = x * 101;
+    this.y = y * 83;
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  }
+  //update(dt) {
+  //  function time(dt){};
+//  }
+  render(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
+}
+
+
+
+class Player {
+  constructor(){
+    this.sprite = 'images/char-boy.png';
+    this.x = 5;
+    this.y = 1;
+  }
+  render(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+
+  handleInput(){
+
+}
+}
+
+const player = new Player();
+const allEnemies = new Enemy();
 
 // Now write your own player class
 // This class requires an update(), render() and
--- js/engine.js
+++ js/engine.js
@@ -65,7 +65,7 @@ var Engine = (function(global) {
    function init() {
         reset();
         lastTime = Date.now();
-        main();
+        //main();
     }
 
     /* This function is called by main (our game loop) and itself calls all
@@ -89,12 +89,12 @@ var Engine = (function(global) {
      * the data/properties related to the object. Do your drawing in your
      * render methods.
      */
-    //function updateEntities(dt) {
+    function updateEntities(dt) {
       //  allEnemies.forEach(function(enemy) {
         //    enemy.update(dt);
       //  });
-    //    player.update();
-  //  }
+        player.update();
+    }
 
     /* This function initially draws the "game level", it will then call
      * the renderEntities function. Remember, this function is called every
@@ -149,11 +149,11 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-        //allEnemies.forEach(function(enemy) {
-          //  enemy.render();
-//        });
+        allEnemies.forEach(function(enemy) {
+            enemy.render();
+        });
 
-        //player.render();
+        player.render();
     }
 
     /* This function does nothing but it could have been a good place to
